1be89fc55fab097834b8c1f52020bf492abcd433
sea <415440369@qq.com>
2022-09-18 00:07:15 +0800

    登录页面

---
+++ SmartParking.Client/SmartParking/SmartParking/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SmartParking
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<Views.MainWindow>();
+        }
+
+        protected override void InitializeShell(Window shell)
+        {
+            if (Container.Resolve<Views.LoginWindow>().ShowDialog() == true)
+                shell.ShowDialog();
+            Application.Current.Shutdown();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
