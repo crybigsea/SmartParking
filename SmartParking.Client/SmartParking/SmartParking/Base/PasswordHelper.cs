20473f19fe149f67a3b32535a7b3caf006a61775
sea <415440369@qq.com>
2022-09-20 17:55:16 +0800

    密码输入框水印处理

---
+++ SmartParking.Client/SmartParking/SmartParking/Base/PasswordHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SmartParking.Base
+{
+    public class PasswordHelper
+    {
+        static bool _isUpdating = false;
+
+        public static string GetPassword(DependencyObject obj)
+        {
+            return (string)obj.GetValue(PasswordProperty);
+        }
+
+        public static void SetPassword(DependencyObject obj, string value)
+        {
+            obj.SetValue(PasswordProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PasswordProperty =
+            DependencyProperty.RegisterAttached("Password", typeof(string), typeof(PasswordHelper),
+                new PropertyMetadata(new PropertyChangedCallback((d, e) =>
+                {
+                    PasswordBox passwordBox = (PasswordBox)d;
+                    passwordBox.PasswordChanged -= PasswordBox_PasswordChanged;
+                    if (!_isUpdating)
+                        passwordBox.Password = e.NewValue.ToString();
+                    passwordBox.PasswordChanged += PasswordBox_PasswordChanged;
+                })));
+
+
+
+        public static int GetAttach(DependencyObject obj)
+        {
+            return (int)obj.GetValue(AttachProperty);
+        }
+
+        public static void SetAttach(DependencyObject obj, int value)
+        {
+            obj.SetValue(AttachProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for Attach.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AttachProperty =
+            DependencyProperty.RegisterAttached("Attach", typeof(int), typeof(PasswordHelper), new PropertyMetadata(new PropertyChangedCallback((d, e) =>
+            {
+                PasswordBox passwordBox = (PasswordBox)d;
+                passwordBox.PasswordChanged += PasswordBox_PasswordChanged;
+            })));
+
+        private static void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            PasswordBox passwordBox = (PasswordBox)sender;
+            _isUpdating = true;
+            SetPassword(passwordBox, passwordBox.Password);
+            _isUpdating = false;
+        }
+    }
+}
--- SmartParking.Client/SmartParking/SmartParking/ViewModels/LoginWindowViewModel.cs
+++ SmartParking.Client/SmartParking/SmartParking/ViewModels/LoginWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,24 @@ using System.Threading.Tasks;
 
 namespace SmartParking.ViewModels
 {
-    public class LoginWindowViewModel
+    public class LoginWindowViewModel : BindableBase
     {
+        private string userName;
+
+        public string UserName
+        {
+            get { return userName; }
+            set { SetProperty(ref userName, value); }
+        }
+
+        private string password = "";
+
+        public string Password
+        {
+            get { return password; }
+            set { SetProperty(ref password, value); }
+        }
+
+
     }
 }
