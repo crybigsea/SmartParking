6e31fac4ee0dd462266ee211807b5e026be22db3
sea <415440369@qq.com>
2022-09-27 15:23:55 +0800

    按钮资源封装

--- SmartParking.Client/SmartParking/SmartParking/Views/LoginWindow.xaml.cs
+++ SmartParking.Client/SmartParking/SmartParking/Views/LoginWindow.xaml.cs
@@ -23,10 +23,5 @@ namespace SmartParking.Views
         {
             InitializeComponent();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            this.DialogResult = true;
-        }
     }
 }
